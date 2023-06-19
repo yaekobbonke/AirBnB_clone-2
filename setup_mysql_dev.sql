7b71c03e62fb1caf735d6654772ec3e2cd084293
yaekobbonke <yaekobbundure>
2023-06-19 04:29:25 -0700

    fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,8 +3,8 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- creates a new user hbnb_dev
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- grants all privileges to database called hbnb_dev_db
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`*.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 -- grants SELLECT privilege to
-GRANT SELECT ON `performance_schema`*.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
