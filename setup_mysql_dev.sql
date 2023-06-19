c0928fd3321172cda1a9c9dae1de393cddee9e3f
yaekobbonke <yaekobbundure>
2023-06-19 04:23:18 -0700

    fix

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,17 +1,9 @@
 -- a script that prepares a MySQL server
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- creates a new user hbnb_dev
-CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- grants all privileges to database called hbnb_dev_db
-GRANT ALL PRIVILEGES
-   ON `hbnb_dev_db`*.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`*.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- grants SELLECT privilege to
-GRANT SELECT
-   ON `performance_schema`*.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON `performance_schema`*.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
