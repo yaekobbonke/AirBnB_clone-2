12b628593cd8bbcdf964f76c74cb0c954c89b48c
yaekobbonke <yaekobbundure>
2023-06-20 21:13:10 -0700

    user fixed for pycode

--- models/user.py
+++ models/user.py
@@ -7,6 +7,7 @@ from sqlalchemy import Column, String, metadata
 
 Base = declarative_base()
 
+
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
@@ -15,5 +16,6 @@ class User(BaseModel, Base):
     password = Column(String(128)),
     first_name = Column(128),
     last_name = Column(128)
+
 
 Base.metadata.create_all(engine)
