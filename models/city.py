5f38145c6ecc8f45bbb7f1dc0eeb089bad8bea0c
yaekobbonke <yaekobbundure>
2023-06-20 21:30:30 -0700

    updated

--- models/city.py
+++ models/city.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from models import base_model
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import String, Culumn
+from sqlalchemy import String, Culumn, metadata
+from sqlalchemy.orm import relationship
 
-
+Base = declarative_base()
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
 
-    name = Column(String(128), nullable=False),
+    name = Column(String(128))
     state_id = Column(String(60), foreinKey('states.id'))
+    places = relationship(Place, backref='City', cascade='delete')
+Base.metadata.create_all(engine)
--- models/user.py
+++ models/user.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from models import base_model
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, metadata
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -12,10 +14,11 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
 
-    email = Column(String(128)),
-    password = Column(String(128)),
-    first_name = Column(128),
+    email = Column(String(128))
+    password = Column(String(128))
+    first_name = Column(128)
     last_name = Column(128)
+    places = relationship(Place, backref='User', cascade='delete')
 
 
 Base.metadata.create_all(engine)
