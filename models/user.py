f77497bb0eead247a1f11ee52309c07f8f9b0d54
yaekobbonke <yaekobbundure>
2023-06-21 01:06:45 -0700

    user updated

--- models/user.py
+++ models/user.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
-from models import base_model
+from models.base_model import Base
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, metadata
 from sqlalchemy.orm import relationship
-
-Base = declarative_base()
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
 
-    email = Column(String(128))
-    password = Column(String(128))
-    first_name = Column(128)
-    last_name = Column(128)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
     places = relationship(Place, backref='User', cascade='delete')
-
-
-Base.metadata.create_all(engine)
+    reviews = relationship(Review, backref='User', cascade='delete')
