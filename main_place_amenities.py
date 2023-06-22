ab4e21a8ce4acdd9e34eab7b6c01c328bcde472a
yaekobbonke <yaekobbundure>
2023-06-21 17:38:27 -0700

    update

--- main_place_amenities.py
+++ main_place_amenities.py
@@ -2,6 +2,7 @@
 """ Test link Many-To-Many Place <> Amenity
 """
 from models import *
+from models.state import State
 
 # creation of a State
 state = State(name="California")
--- models/base_model.py
+++ models/base_model.py
@@ -19,17 +19,25 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            kwargs[updated_at] = datetime.strptime(kwargs[updated_at],
-                                                   '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs[created_at] = datetime.strptime(kwargs[created_at],
-                                                   '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        #if not kwargs:
+          #  self.id = str(uuid.uuid4())
+         #   self.created_at = datetime.now()
+        #    self.updated_at = datetime.now()
+       # else:
+           # kwargs[updated_at] = datetime.strptime(kwargs[updated_at],
+             #                                      '%Y-%m-%dT%H:%M:%S.%f')
+            #kwargs[created_at] = datetime.strptime(kwargs[created_at],
+             #                                      '%Y-%m-%dT%H:%M:%S.%f')
+            #del kwargs['__class__']
+           # self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
