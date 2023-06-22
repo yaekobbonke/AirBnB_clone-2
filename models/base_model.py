bd58233d35fcadb56db899cf7495e665f4acef64
yaekobbonke <yaekobbundure>
2023-06-21 17:08:16 -0700

    pycodestyle check

--- models/base_model.py
+++ models/base_model.py
@@ -25,9 +25,9 @@ class BaseModel:
             self.updated_at = datetime.now()
         else:
             kwargs[updated_at] = datetime.strptime(kwargs[updated_at],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+                                                   '%Y-%m-%dT%H:%M:%S.%f')
             kwargs[created_at] = datetime.strptime(kwargs[created_at],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+                                                   '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
