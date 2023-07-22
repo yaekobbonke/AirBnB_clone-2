b1a99447627b1473c24c015f886cefe55ce9d242
yaekobbonke <yaekobbundure@gmail.com>
2023-07-22 03:15:42 +0000

    fixed

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_HBNB():
-    """ prints hello_HBNB"""
-    return HELLO HBNB
+    """ prints hello_HBNB """
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
