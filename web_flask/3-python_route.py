fad9ba108ceeaa9980bd05b416df8570bc1da397
yaekobbonke <yaekobbundure@gmail.com>
2023-07-22 10:20:21 +0000

    fix

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -7,7 +7,7 @@ Routes:
     /hbnb: display "HBNB"
     /c/<text>: display “C ”, followed by the value of the text variable
      replace underscore _ symbols with a space
-     /python/(<text>):display “Python ”, followed by 
+     /python/(<text>):display “Python”, followed by
      the value of the text variable:
      replace underscore _ symbols with a space
      The default value of text is “is cool”
