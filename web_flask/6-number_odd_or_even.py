6c344690e8ef160c220024c0f6127ca360ad630f
yaekobbonke <yaekobbundure@gmail.com>
2023-07-23 12:28:45 +0000

    fixed

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -29,7 +29,7 @@ def c(text):
     """Displays 'C' followed by the value of <text>.
 
     Replaces any underscores in <text> with slashes.
-    """
+      """
     text = text.replace("_", " ")
     return "C {}".format(text)
 
@@ -57,8 +57,13 @@ def number_template(n):
     return render_template("5-number.html", n=n)
 
 
-@app.route("/number_odd_or_even/<int:n>")
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
+    """
+     display a HTML page only if n is an integer:
+    """
+    return render_template("6-number_odd_or_even.html", n=n)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
