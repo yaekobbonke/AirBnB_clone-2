053426b9ff798ed0494dad38f26e010c78998ab6
yaekobbonke <yaekobbundure@gmail.com>
2023-07-23 06:45:44 +0000

    fixed

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -45,8 +45,8 @@ def python(text="is cool"):
     return "Python {}".format(text)
 
 
-@app.route("/number/<n>", strict_slashes=False)
-def n_number(n):
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
     """display “n is a number” only if n is an integer"""
     return "{} is a number".format(n)
 
